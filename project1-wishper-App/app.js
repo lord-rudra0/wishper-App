b6c27f5cb7effbea684a359c2fe9322571a84656
rudra pratap singh <prataprudra10102001@gmail.com>
2025-02-19 19:18:17 +0530

    Register success

--- project1-wishper-App/app.js
+++ project1-wishper-App/app.js
@@ -1,6 +1,9 @@
 import express from "express"
 import bodyParser from "body-parser";
+import mongoose from "mongoose"
 const port = 5000;
+
+const mongoURI = "mongodb://localhost:27017/userDB";
 
 
 const app = express();
@@ -10,6 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
 app.get("/", function (req, res) {
     res.render("home");
 });
@@ -18,13 +22,50 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
+app.post("/register", async function (req, res) {
+    try {
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password,
+        });
+        await newUser.save()
+        res.render("secrets")
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+
 
 app.get("/login", function (req, res) {
     res.render("login");
 });
 
 
+async function connectDB() {
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connected successfully ");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+}
 
+
+connectDB();
+
+
+const userScema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const User = mongoose.model("User", userScema)
 
 
 app.listen(port, function () {
